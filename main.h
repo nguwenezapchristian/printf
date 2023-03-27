6f9d45df6becbc2162d39e69c19c8b49f19c6791
Christelle Alexia <christellealexia53@gmail.com>
2023-03-27 15:31:02 -0700

    added prototypes

--- main.h
+++ main.h
@@ -21,4 +21,12 @@ typedef struct print
 
 int _printf(const char *format, ...);
 
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+
+int ev_print_func(const char *s, int index);
+
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
+
+int print_buf(char *buf, unsigned int nbuf);
+
 #endif
